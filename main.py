2de7d0623ff94a5a2fb4d4a30d2e7ae03b212d52
annes <anneswidow@gmail.com>
2026-03-23 17:46:22 -0500

    e

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ import player
 me = player.Player()
 cursor = False
 
-e = rl.load_model("assets/models/planet.glb")
+e = None
 
 def main():
-    global cursor
+    global cursor, e
     rl.set_config_flags(rl.FLAG_WINDOW_RESIZABLE)
     rl.init_window(title="Minecraft 2")
     rl.set_target_fps(60)
     rl.init_audio_device()
     rl.disable_cursor()
+
+    e =rl.load_model("assets/models/planet.glb")
+    r = rl.load_texture("assets/textures/planet/ne_kleki.png")
 
     while not rl.window_should_close():
         if rl.is_key_pressed(rl.KEY_M):
@@ -25,7 +28,7 @@ def main():
             cursor = not cursor
 
         rl.begin_drawing()
-        space.draw_space(me)
+        space.draw_space(me, e, r)
         rl.end_drawing()
     rl.close_audio_device()
     rl.close_window()
--- space.py
+++ space.py
@@ -1,10 +1,13 @@
 import ultimateraylib as rl
 import player
-def draw_space(player: player.Player):
+def draw_space(player: player.Player, e:rl.Model, r:rl.Texture2D):
     player.update()
     rl.clear_background(rl.BLACK)
 
     rl.begin_mode_3d(player.camera)
     rl.draw_grid(10, 1)
-    rl.draw_cube(rl.Vector3(0,0,0), 1, 1, 1, rl.WHITE)
+    #rl.draw_cube(rl.Vector3(0,0,0), 1, 1, 1, rl.WHITE)
+    
+    e.materials[0].maps[rl.MATERIAL_MAP_ALBEDO[0]].texture = r # pyright: ignore[reportIndexIssue]
+    rl.draw_model(e, rl.Vector3(0,0,0), 1, rl.WHITE)
     rl.end_mode_3d()
