2e97398eb232fa6536cf80f27125c7b6e8b60fd4
annes <anneswidow@gmail.com>
2026-03-25 19:47:25 -0500

    universe

--- space.py
+++ space.py
@@ -5,7 +5,7 @@ def draw_space(player: player.Player, e:rl.Model, r:rl.Texture2D):
     rl.clear_background(rl.BLACK)
 
     rl.begin_mode_3d(player.camera)
-    rl.draw_grid(10, 1)
+    rl.draw_grid(50, 1)
     #rl.draw_cube(rl.Vector3(0,0,0), 1, 1, 1, rl.WHITE)
     
     e.materials[0].maps[rl.MATERIAL_MAP_ALBEDO[0]].texture = r # pyright: ignore[reportIndexIssue]
